fb3f3c242eb31287d64707ede8c750c419913af1
ro1677 <robinss7412@gmail.com>
2025-03-07 00:55:20 +0900

    fix: add missing dependencies and update build settings

---
+++ catpresso/pages/_app.js
@@ -0,0 +1,29 @@
+import { ConnectionProvider, WalletProvider } from "@solana/wallet-adapter-react";
+import { WalletModalProvider } from "@solana/wallet-adapter-react-ui";
+import { PhantomWalletAdapter } from "@solana/wallet-adapter-wallets";
+import { clusterApiUrl } from "@solana/web3.js";
+import { useMemo } from "react";
+
+import "../styles/globals.css";
+
+require("@solana/wallet-adapter-react-ui/styles.css");
+
+function MyApp({ Component, pageProps }) {
+  const network = "devnet";
+  const endpoint = useMemo(() => clusterApiUrl(network), [network]);
+
+  const wallets = useMemo(() => [new PhantomWalletAdapter()], []);
+
+  return (
+    <ConnectionProvider endpoint={endpoint}>
+      <WalletProvider wallets={wallets} autoConnect>
+        <WalletModalProvider>
+          <Component {...pageProps} />
+        </WalletModalProvider>
+      </WalletProvider>
+    </ConnectionProvider>
+  );
+}
+
+export default MyApp;
+
