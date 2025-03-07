fb239770b6515842bd30d54065fc43cd5b942b32
ro1677 <robinss7412@gmail.com>
2025-03-08 05:50:32 +0900

    fix: add missing dependencies and update build settings

--- catpresso/components/BackgroundInfo.js
+++ catpresso/components/BackgroundInfo.js
@@ -5,10 +5,10 @@ export default function BackgroundInfo({ selectedLanguage }) {
       <div className="text-center text-black">
         {selectedLanguage === "ko" ? (
           <>
-            <h1 className="text-2xl font-bold">= Catpresso Cho-cho-ro 멤버쉽 밈코인</h1>
-            <p>[카페 멤버쉽, AI GPT, Castles of pepe 코인 채굴 오픈 예정]</p>
+            <h1 className="text-2xl font-bold">= Catpresso Cho-cho-ro 밈코인</h1>
+            <p>[카페 멤버쉽, AI GPT 오픈 예정]</p>
             <p>
-              캣프레소 토큰은 카페 멤버십, AI GPT 서비스, 캐슬오브페페 토큰 채굴 등 다양한 디지털 서비스를 이용할 수 있는 멤버십 토큰입니다.
+              캣프레소 토큰은 카페 멤버십, AI GPT 서비스 등 다양한 디지털 서비스를 이용할 수 있는 멤버십 토큰입니다.
             </p>
             <p className="mt-4">
               * 지갑이 없으신 경우, 수량과 이메일 입력 후, 원화 구매 버튼을 누르십시오. 토큰이 이메일 주소로 발송됩니다.
